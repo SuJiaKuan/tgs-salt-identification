123e384b0dc918006426b07b9ed2a1df1c19b7dc
SuJiaKuan <feabries@gmail.com>
2018-09-17 18:06:30 +0800

    Update configuration

--- src/config.py
+++ src/config.py
@@ -1,12 +1,10 @@
 import os
 
-# DATA_ROOT_DIR = '/data/feabries/tgs-salt-identification-challenge'
 DATA_ROOT_DIR = '../input'
 TRAIN_DF_PATH = os.path.join(DATA_ROOT_DIR, 'train.csv')
 TRAIN_IMGS_DIR = os.path.join(DATA_ROOT_DIR, 'train/images')
 TRAIN_MASKS_DIR = os.path.join(DATA_ROOT_DIR, 'train/masks')
-# TEST_MASKS_DIR = os.path.join(DATA_ROOT_DIR, 'test/images')
-TEST_IMGS_DIR = os.path.join(DATA_ROOT_DIR, 'train/images')
+TEST_MASKS_DIR = os.path.join(DATA_ROOT_DIR, 'test/images')
 
 MODELS_DIR = '../models'
 
